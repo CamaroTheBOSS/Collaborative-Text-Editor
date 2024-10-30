e08068e411782927103ebe400f5e24e0a9eaa7db
CamaroTheBOSS <plessico@wp.pl>
2024-10-30 18:05:27 +0100

    Replaced legacy rendering tests with screen buffer tests

--- Test/screen_buffer_test.cpp
+++ Test/screen_buffer_test.cpp
@@ -3,24 +3,17 @@
 #include "renderer.h"
 
 
-ScreenBuffer getScreenBuffer() {
-	ScreenBuffer screenBuffer;
-	screenBuffer.srWindow = SMALL_RECT(0, 0, 20, 20);
-	screenBuffer.dwSize = COORD{ 20, 20 };
-	return screenBuffer;
-}
-
 
 TEST(RendererTests, CursorSyncOneLinerDocTest) {
 	Document doc{ "Oneliner test", 1, 0 };
 	COORD dPos{ 5, 0 };
 	doc.setCursorPos(0, dPos);
 
-	Renderer renderer;
-	ScreenBuffer sb = getScreenBuffer();
-	COORD tPos = renderer.synchronizeCursor(doc, dPos, sb);
-	EXPECT_EQ(tPos.X, 5);
-	EXPECT_EQ(tPos.Y, 0);
+	ScrollableScreenBuffer screenBuffer{ SMALL_RECT(0, 0, 20, 20) };
+	Cursor tCursor = screenBuffer.getMyTerminalCursor(doc);
+	EXPECT_EQ(tCursor.pointedChar, 'n');
+	EXPECT_EQ(tCursor.pos.X, 5);
+	EXPECT_EQ(tCursor.pos.Y, 0);
 }
 
 
@@ -29,11 +22,11 @@ TEST(RendererTests, CursorSyncOneEmptyLineTest) {
 	COORD dPos{ 0, 1 };
 	doc.setCursorPos(0, dPos);
 
-	Renderer renderer;
-	ScreenBuffer sb = getScreenBuffer();
-	COORD tPos = renderer.synchronizeCursor(doc, dPos, sb);
-	EXPECT_EQ(tPos.X, 0);
-	EXPECT_EQ(tPos.Y, 1);
+	ScrollableScreenBuffer screenBuffer{ SMALL_RECT(0, 0, 20, 20) };
+	Cursor tCursor = screenBuffer.getMyTerminalCursor(doc);
+	EXPECT_EQ(tCursor.pointedChar, ' ');
+	EXPECT_EQ(tCursor.pos.X, 0);
+	EXPECT_EQ(tCursor.pos.Y, 1);
 }
 
 
@@ -42,11 +35,11 @@ TEST(RendererTests, CursorSyncOneLinerFilledTest) {
 	COORD dPos{ 20, 0 };
 	doc.setCursorPos(0, dPos);
 
-	Renderer renderer;
-	ScreenBuffer sb = getScreenBuffer();
-	COORD tPos = renderer.synchronizeCursor(doc, dPos, sb);
-	EXPECT_EQ(tPos.X, 0);
-	EXPECT_EQ(tPos.Y, 1);
+	ScrollableScreenBuffer screenBuffer{ SMALL_RECT(0, 0, 20, 20) };
+	Cursor tCursor = screenBuffer.getMyTerminalCursor(doc);
+	EXPECT_EQ(tCursor.pointedChar, ' ');
+	EXPECT_EQ(tCursor.pos.X, 0);
+	EXPECT_EQ(tCursor.pos.Y, 1);
 	// doc's first line is filled with text, so cursor at the end should jump to the start of next line
 }
 
@@ -56,11 +49,11 @@ TEST(RendererTests, CursorSyncOneLinerFilledWithEndlTest) {
 	COORD dPos{ 20, 0 };
 	doc.setCursorPos(0, dPos);
 
-	Renderer renderer;
-	ScreenBuffer sb = getScreenBuffer();
-	COORD tPos = renderer.synchronizeCursor(doc, dPos, sb);
-	EXPECT_EQ(tPos.X, 0);
-	EXPECT_EQ(tPos.Y, 1);
+	ScrollableScreenBuffer screenBuffer{ SMALL_RECT(0, 0, 20, 20) };
+	Cursor tCursor = screenBuffer.getMyTerminalCursor(doc);
+	EXPECT_EQ(tCursor.pointedChar, ' ');
+	EXPECT_EQ(tCursor.pos.X, 0);
+	EXPECT_EQ(tCursor.pos.Y, 1);
 	// doc's first line is filled with text and endl, so cursor at the end should jump to the start of next line
 	// there was a bug with this case in the past
 }
@@ -68,15 +61,16 @@ TEST(RendererTests, CursorSyncOneLinerFilledWithEndlTest) {
 
 TEST(RendererTests, CursorSyncForLongerLinesTest) {
 	Document doc{ "This is oneline but longer than screen width\n", 4, 0 };
-	ScreenBuffer sb = getScreenBuffer();
-	Renderer renderer;
-
+	ScrollableScreenBuffer screenBuffer{ SMALL_RECT(0, 0, 20, 20) };
+	
 	std::vector<COORD> testDocPositions = { {5, 0}, {25, 0}, {45, 0}, {0, 1} };
 	std::vector<COORD> desiredTerminalPositions = { {5, 0}, {5, 1}, {5, 2}, {0, 3} };
+	std::vector<char> desiredPointedChars = { 'i', 'r', ' ', ' ' };
 	for (int i = 0; i < testDocPositions.size(); i++) {
 		doc.setCursorPos(i, testDocPositions[i]);
-		COORD tPos = renderer.synchronizeCursor(doc, testDocPositions[i], sb);
-		EXPECT_EQ(tPos.X, desiredTerminalPositions[i].X);
-		EXPECT_EQ(tPos.Y, desiredTerminalPositions[i].Y);
+		Cursor tCursor = screenBuffer.getTerminalCursor(doc, i);
+		EXPECT_EQ(tCursor.pointedChar, desiredPointedChars[i]);
+		EXPECT_EQ(tCursor.pos.X, desiredTerminalPositions[i].X);
+		EXPECT_EQ(tCursor.pos.Y, desiredTerminalPositions[i].Y);
 	}
 }
