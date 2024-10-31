fead36cbdd67e09b5e2d55e8249aa907233006c7
CamaroTheBOSS <plessico@wp.pl>
2024-10-31 18:55:14 +0100

    Improved framer test

--- Test/framer_test.cpp
+++ Test/framer_test.cpp
@@ -1,7 +1,25 @@
 #include "pch.h"
 #include "framer.h"
 
+const std::string strWithPrimarSize = "unbeliveable very long and large";
 const std::string testStr = "test string";
+const std::string largeTestStr = R"(
+	unbeliveable very long and large test string what the hell is that I cannot belive that :O.
+	unbeliveable very long and large test string what the hell is that I cannot belive that :O.
+	unbeliveable very long and large test string what the hell is that I cannot belive that :O.
+	unbeliveable very long and large test string what the hell is that I cannot belive that :O.
+	unbeliveable very long and large test string what the hell is that I cannot belive that :O.
+	unbeliveable very long and large test string what the hell is that I cannot belive that :O.
+	unbeliveable very long and large test string what the hell is that I cannot belive that :O.
+	unbeliveable very long and large test string what the hell is that I cannot belive that :O.
+)";
+
+msg::Buffer prepPrimarTestMsg() {
+	msg::Buffer msg{37};
+	msg::serializeTo(msg, 0, strWithPrimarSize);
+	msg::Buffer msgWithSize = msg::enrich(msg);
+	return msgWithSize;
+}
 
 msg::Buffer prepTestMsg() {
 	msg::Buffer msg{16};
@@ -10,15 +28,31 @@ msg::Buffer prepTestMsg() {
 	return msgWithSize;
 }
 
-void testMsgExtraction(Framer& framer, msg::Buffer& buffer, const int expectedMsgs ) {
-	
+msg::Buffer prepLargeTestMsg() {
+	msg::Buffer msg{1000};
+	msg::serializeTo(msg, 0, largeTestStr);
+	msg::Buffer msgWithSize = msg::enrich(msg);
+	return msgWithSize;
+}
+
+std::vector<std::string> extractStrings(Framer& framer, msg::Buffer& buffer ) {
+	std::vector<std::string> messages;
 	auto separatedMessages = framer.extractMessages(buffer);
-	EXPECT_EQ(separatedMessages.size(), expectedMsgs);
 	for (auto& message : separatedMessages) {
 		std::string parsedStr;
 		msg::parse(message, 0, parsedStr);
-		EXPECT_EQ(message.size, testStr.size() + 1);
-		EXPECT_EQ(parsedStr, testStr);
+		messages.emplace_back(parsedStr);
+	}
+	return messages;
+}
+
+void testMsgs(const std::vector<std::string>& actual, const std::vector<std::string> expected) {
+	EXPECT_EQ(actual.size(), expected.size());
+	if (actual.size() != expected.size()) {
+		return;
+	}
+	for (int i = 0; i < actual.size(); i++) {
+		EXPECT_EQ(actual[i], expected[i]);
 	}
 }
 
@@ -27,7 +61,8 @@ TEST(FramerTests, ExtractingThreeMessagesTest) {
 	msg::Buffer bigBuffer{48};
 	auto msgWithSize = prepTestMsg();
 	msg::serializeTo(bigBuffer, 0, msgWithSize, msgWithSize, msgWithSize);
-	testMsgExtraction(framer, bigBuffer, 3);
+	auto msgs = extractStrings(framer, bigBuffer);
+	testMsgs(msgs, { testStr, testStr, testStr });
 }
 
 TEST(FramerTests, ExtractingIncompleteMessageTest) {
@@ -39,21 +74,61 @@ TEST(FramerTests, ExtractingIncompleteMessageTest) {
 	firstBigBuffer.add(&msgWithSize, 0, 10);
 	secondBigBuffer.add(&msgWithSize, 10, msgWithSize.size - 10);
 	msg::serializeTo(secondBigBuffer, 0, msgWithSize);
-
-	testMsgExtraction(framer, firstBigBuffer, 2);
-	testMsgExtraction(framer, secondBigBuffer, 2);
+	auto firstMsgs = extractStrings(framer, firstBigBuffer);
+	auto secondMsgs = extractStrings(framer, secondBigBuffer);
+	testMsgs(firstMsgs, { testStr, testStr });
+	testMsgs(secondMsgs, { testStr, testStr });
 }
 
 TEST(FramerTests, ExtractingMessageWithIncompleteLengthTest) {
-	Framer framer{ 128 };
-	msg::Buffer firstBigBuffer{34};
-	msg::Buffer secondBigBuffer{34};
-	auto msgWithSize = prepTestMsg();
-	msg::serializeTo(firstBigBuffer, 0, msgWithSize, msgWithSize);
-	firstBigBuffer.add(&msgWithSize, 0, 2);
-	secondBigBuffer.add(&msgWithSize, 2, msgWithSize.size - 2);
-	msg::serializeTo(secondBigBuffer, 0, msgWithSize);
+	Framer framer{ 1024 };
+	int nMessages = 5;
+	auto msg = prepTestMsg();
+	auto largeMsg = prepLargeTestMsg();
+	for (int i = 0; i < 4; i++) {
+		msg::Buffer firstBigBuffer{nMessages * 16 + 1 + i};
+		msg::Buffer secondBigBuffer{1000};
+		for (int j = 0; j < nMessages; j++) {
+			firstBigBuffer.add(&msg);
+		}
+		int anchor = firstBigBuffer.capacity - firstBigBuffer.size;
+		firstBigBuffer.add(&largeMsg, 0, anchor);
+		secondBigBuffer.add(&largeMsg, anchor, largeMsg.size - anchor);
+		msg::serializeTo(secondBigBuffer, largeMsg.size - anchor, msg, msg);
+		auto firstMsgs = extractStrings(framer, firstBigBuffer);
+		auto secondMsgs = extractStrings(framer, secondBigBuffer);
+		testMsgs(firstMsgs, { testStr, testStr, testStr, testStr, testStr });
+		testMsgs(secondMsgs, { largeTestStr, testStr, testStr });
+	}
+	
+}
 
-	testMsgExtraction(framer, firstBigBuffer, 2);
-	testMsgExtraction(framer, secondBigBuffer, 2);
+TEST(FramerTests, ContinousExtractionTest) {
+	Framer framer{ 128 };
+	constexpr int bufferSize = 10000;
+	constexpr int fragmentation = 128;	
+
+	auto msg = prepPrimarTestMsg();
+	const int expectedNMsgs = bufferSize / msg.size;
+	msg::Buffer continousBuffer{bufferSize};
+	for (int i = 0; i < expectedNMsgs; i++) {
+		continousBuffer.add(&msg);
+	}
+	
+	int nMsgs = 0;
+	int head = 0;
+	int remainingSymbols = 0;
+	do {
+		msg::Buffer buffer{fragmentation};
+		remainingSymbols = continousBuffer.size - head;
+		int nSymbols = (std::min)(remainingSymbols, fragmentation);
+		buffer.add(&continousBuffer, head, nSymbols);
+		auto extractedMsgs = extractStrings(framer, buffer);
+		nMsgs += extractedMsgs.size();
+		for (const auto& msg : extractedMsgs) {
+			EXPECT_EQ(msg, strWithPrimarSize);
+		}
+		head += nSymbols;
+	} while (remainingSymbols);
+	EXPECT_EQ(nMsgs, expectedNMsgs);
 }
