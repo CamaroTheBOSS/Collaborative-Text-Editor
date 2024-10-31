658f9e6ade7e6bd8ab6d9acc64b9ac7e5e7fbb3b
CamaroTheBOSS <plessico@wp.pl>
2024-10-31 18:19:56 +0100

    Revert "Framer now will drop messages which are longer than its capacity"
    This reverts commit ead2c6a721ad1455897e3f7b590fe386cb7f8d70.

--- Document/framer.cpp
+++ Document/framer.cpp
@@ -3,63 +3,37 @@
 Framer::Framer(const int capacity) :
 	workBuff(capacity) {}
 
-Messages Framer::extractMessages(msg::Buffer& recvBuff) {
-	Messages messages;
-	if (mode == Mode::frame) {
-		return frame(0, recvBuff, messages);
-	}
-	return drop(0, recvBuff, messages);
-}
+std::vector<msg::Buffer> Framer::extractMessages(msg::Buffer& recvBuff) {
+	int head = 0;
+	std::vector<msg::Buffer> messages;
+	int remainingSymbols = 0;
+	while ((remainingSymbols = recvBuff.size - head) > 0) {
+		if (remainingSymbols < 4) {
+			workBuff.add(&recvBuff, head, remainingSymbols);
+			break;
+		}
 
-Messages Framer::drop(int head, msg::Buffer& recvBuff, Messages& messages) {
-	int remainingSymbols = recvBuff.size - head;
-	if (neededSymbols < remainingSymbols) {
+		int msgSizeRemainingSymbols = 4 - workBuff.size;
+		if (msgSizeRemainingSymbols > 0) {
+			workBuff.add(&recvBuff, head, msgSizeRemainingSymbols);
+			msg::parse(workBuff, 0, neededSymbols);
+			head += msgSizeRemainingSymbols;
+			remainingSymbols -= msgSizeRemainingSymbols;
+		}
+			
+		if (remainingSymbols < neededSymbols) {
+			workBuff.add(&recvBuff, head, remainingSymbols);
+			neededSymbols -= remainingSymbols;
+			break;
+		}
+
+		workBuff.add(&recvBuff, head, neededSymbols);
+		messages.emplace_back(msg::Buffer{static_cast<int>(workBuff.size) - 4});
+		messages[messages.size() - 1].add(&workBuff, 4, workBuff.size - 4);
+
 		head += neededSymbols;
 		neededSymbols = 0;
-		mode = Mode::frame;
 		workBuff.clear();
-		return frame(head, recvBuff, messages);
 	}
-	neededSymbols -= remainingSymbols;
 	return messages;
-}
-
-
-Messages Framer::frame(int head, msg::Buffer& recvBuff, Messages& messages) {
-	int remainingSymbols = recvBuff.size - head;
-	if (remainingSymbols == 0) {
-		return messages;
-	}
-	if (remainingSymbols < 4) {
-		workBuff.add(&recvBuff, head, remainingSymbols);
-		return messages;
-	}
-
-	int msgSizeRemainingSymbols = 4 - workBuff.size;
-	if (msgSizeRemainingSymbols > 0) {
-		workBuff.add(&recvBuff, head, msgSizeRemainingSymbols);
-		msg::parse(workBuff, 0, neededSymbols);
-		head += msgSizeRemainingSymbols;
-		remainingSymbols -= msgSizeRemainingSymbols;
-	}
-	if (neededSymbols > workBuff.capacity - 4) {
-		mode = Mode::drop;
-		workBuff.clear();
-		return drop(head, recvBuff, messages);
-	}
-
-	if (remainingSymbols < neededSymbols) {
-		workBuff.add(&recvBuff, head, remainingSymbols);
-		neededSymbols -= remainingSymbols;
-		return messages;
-	}
-
-	workBuff.add(&recvBuff, head, neededSymbols);
-	messages.emplace_back(msg::Buffer{static_cast<int>(workBuff.size) - 4});
-	messages[messages.size() - 1].add(&workBuff, 4, workBuff.size - 4);
-
-	head += neededSymbols;
-	neededSymbols = 0;
-	workBuff.clear();
-	return frame(head, recvBuff, messages);
 }
--- Document/framer.h
+++ Document/framer.h
@@ -3,19 +3,11 @@
 
 #include "messages.h"
 
-using Messages = std::vector<msg::Buffer>;
-
 class Framer {
 public:
 	Framer(const int capacity);
-	Messages extractMessages(msg::Buffer& recvBuff);
+	std::vector<msg::Buffer> extractMessages(msg::Buffer& recvBuff);
 private:
-	enum class Mode {frame, drop};
-
-	Messages drop(int startPos, msg::Buffer& recvBuff, Messages& messages);
-	Messages frame(int startPos, msg::Buffer& recvBuff, Messages& messages);
-
-	Mode mode = Mode::frame;
 	msg::Buffer workBuff;
 	unsigned int neededSymbols = 0;
 };
