c9edcaf72dd76bc05b362b7d2263c00e3d920d04
CamaroTheBOSS <plesiakmail@gmail.com>
2026-09-18 22:49:03 +0200

    improved python script

--- scripts/client.py
+++ scripts/client.py
@@ -8,20 +8,25 @@ def dir_path(string):
     if os.path.exists(string):
         return string
     else:
-        raise NotADirectoryError(string)
+        raise FileNotFoundError(string)
 
 
 print(os.getcwd())
 parser = argparse.ArgumentParser()
-parser.add_argument("--command", default="run", help="entrypoint command for the software (run/create/join/register)")
-parser.add_argument("--ip", default="192.168.1.10", help="ip of the server")
-parser.add_argument("--port", default="8081", help="port of the server")
-parser.add_argument("--login", help="comma separated logins to the users login1,login2,login3")
-parser.add_argument("--password", help="comma separated passwords to the users pass1,pass2,pass3")
-parser.add_argument("--access-code", default="x", help="access code for joining session")
-parser.add_argument("--exe-path", default="Client.exe", type=dir_path, help="path to Client.exe")
+parser.add_argument("--command", default="", help="Entrypoint command for the software (run/create/join/register). Default: ''")
+parser.add_argument("--ip", default="127.0.0.1", help="IP of the server. Default: '127.0.0.1'")
+parser.add_argument("--port", default="8081", help="Port of the server. Default: '8081'")
+parser.add_argument("--login", help="Comma separated logins to the users login1,login2,login3")
+parser.add_argument("--password", help="Comma separated passwords to the users pass1,pass2,pass3")
+parser.add_argument("--access-code", default="", help="Access code for joining session")
+parser.add_argument("--exe-path", default="./ClientMain.exe", type=dir_path, help="Path to client executable. May be ClientMain.exe or RandomClient.exe. Default: './ClientMain.exe'")
 
-args = parser.parse_args()
+try:
+    args = parser.parse_args()
+except FileNotFoundError as file:
+    print(f"\nERROR: Cannot find provided file: {file}.\n")
+    parser.print_help()
+    exit(1)
 passwords = args.password.split(",")
 logins = args.login.split(",")
 size = min(len(passwords), len(logins))
